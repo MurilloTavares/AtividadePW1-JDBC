591ec7898ebcd84590bb23205f0ceb600eccea71
Murillo Tavares <murillo_eum@hotmail.com>
2018-06-03 16:46:04 -0300

    Add Classes Cliente e Pedido

---
+++ AtividadePW1-JDBC/src/main/java/modelo/Cliente.java
@@ -0,0 +1,136 @@
+package modelo;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Cliente {
+    
+    private int id;
+    private String nome;
+    private String documento;
+    private float saldo;
+    private boolean ativo;
+    
+    private File imagem;
+
+    public Cliente(int id, String nome, String documento, float saldo, boolean ativo, String imgPath) {
+        this.id = id;
+        this.nome = nome;
+        this.documento = documento;
+        this.saldo = saldo;
+        this.ativo = ativo;
+        this.imagem = new File(imgPath);
+    }
+
+    public Cliente(int id, String nome, String documento, float saldo, boolean ativo) {
+        this.id = id;
+        this.nome = nome;
+        this.documento = documento;
+        this.saldo = saldo;
+        this.ativo = ativo;
+        this.imagem = new File("Default.img");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public File getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(File imagem) {
+        this.imagem = imagem;
+    }
+    
+    public void setImagem(String imgPath) {
+        this.imagem = new File (imgPath);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + this.id;
+        hash = 47 * hash + Objects.hashCode(this.nome);
+        hash = 47 * hash + Objects.hashCode(this.documento);
+        hash = 47 * hash + Float.floatToIntBits(this.saldo);
+        hash = 47 * hash + (this.ativo ? 1 : 0);
+        hash = 47 * hash + Objects.hashCode(this.imagem);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.saldo) != Float.floatToIntBits(other.saldo)) {
+            return false;
+        }
+        if (this.ativo != other.ativo) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.documento, other.documento)) {
+            return false;
+        }
+        if (!Objects.equals(this.imagem, other.imagem)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nome=" + nome + ", documento=" + documento + ", saldo=" + saldo + ", ativo=" + ativo + ", imagem=" + imagem + '}';
+    }
+    
+}
---
+++ AtividadePW1-JDBC/src/main/java/modelo/Pedido.java
@@ -0,0 +1,94 @@
+package modelo;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class Pedido {
+
+    private int id;
+    private Date data;
+    private int cliente;
+    private float valor;
+
+    public Pedido(int id, Date data, int cliente, float valor) {
+        this.id = id;
+        this.data = data;
+        this.cliente = cliente;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(int cliente) {
+        this.cliente = cliente;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + this.id;
+        hash = 47 * hash + Objects.hashCode(this.data);
+        hash = 47 * hash + this.cliente;
+        hash = 47 * hash + Float.floatToIntBits(this.valor);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pedido other = (Pedido) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.cliente != other.cliente) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.valor) != Float.floatToIntBits(other.valor)) {
+            return false;
+        }
+        if (!Objects.equals(this.data, other.data)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" + "id=" + id + ", data=" + data + ", cliente=" + cliente + ", valor=" + valor + '}';
+    }
+    
+}
